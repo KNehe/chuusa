565ea1af5824b8b75980ef9e8a9a70ae3887af85
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-04-20 15:56:31 +0300

    Design

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.wrapper}>
       <Head>
-        <title>Create Next App</title>
+        <title>Chuusa</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <section className={styles.left}>
+          <header className={styles.header}>
+            <img src="undraw_Filing_system_re_56h6.svg" alt="logo"/>
+            <h1>Chuusa</h1>
+          </header>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <article className={styles.info}>
+            <h2>Convert your word documents to pdf</h2>
+            <p>Pay $0</p>
+          </article>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <article className={styles.input}>
+            <p>Drop a file here or </p>
+            <label htmlFor="fileInput" className={styles.input_label}>Browse file</label>
+            <input 
+              type='file'
+              id="fileInput" 
+              accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              multiple={false}/>              
+          </article>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <article className={styles.message}>
+            <p className={styles.error}>Error message</p>
+            <p className={styles.success}>Sucess message
+             <a href="#" download>Download</a>
+             </p>
+          </article>
+        </section> 
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section className={styles.right}>
+          <img src="collecting.svg" alt=""/>                 
+        </section>     
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    
     </div>
   )
 }
